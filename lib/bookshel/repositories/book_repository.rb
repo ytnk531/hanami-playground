fd634f4e8ff0ef401ab7b3d5f3aec0de10ec217e
Yudai Tanaka <ytnk531@gmail.com>
2019-12-21 20:32:12 +0900

    Add repository tests

--- lib/bookshel/repositories/book_repository.rb
+++ lib/bookshel/repositories/book_repository.rb
@@ -1,3 +1,12 @@
 class BookRepository < Hanami::Repository
-    
+  def by_title(title)
+    books
+      .where(title: title)
+      .order(:created_at)
+      .first
+  end
+
+  def create_many(data)
+    command(:create, books, use: [:timestamps], result: :many).call(data)
+  end
 end
--- spec/bookshel/repositories/book_repository_spec.rb
+++ spec/bookshel/repositories/book_repository_spec.rb
@@ -1,3 +1,42 @@
+# frozen_string_literal: true
+
 RSpec.describe BookRepository, type: :repository do
-  # place your tests here
+  let(:tested) { described_class.new }
+
+  before { tested.clear }
+
+  describe '#by_title' do
+    subject { tested.by_title(title) }
+    let!(:title) { 'target' }
+
+    let!(:matched_book) do
+      tested.create Book.new(title: 'target', author: 'author1')
+    end
+    let!(:unmatched_book) { tested.create Book.new(title: 'title3', author: 'author2') }
+
+    it { is_expected.to eq matched_book }
+
+    context 'when there are multiple same titled books' do
+      let!(:title) { 'multiple' }
+
+      let!(:older) { tested.create Book.new(title: 'multiple', author: 'author3') }
+      let!(:newer) { tested.create Book.new(title: 'multiple', author: 'author3') }
+
+      it { is_expected.to eq older }
+    end
+  end
+
+  describe '#create_many' do
+    subject { tested.create_many(books) }
+
+    let(:books) do
+      [
+        Book.new(title: 'title', author: 'author'),
+        Book.new(title: 'title', author: 'author'),
+        Book.new(title: 'title', author: 'author')
+      ]
+    end
+
+    it { expect { subject }.to change { tested.all.size }.from(0).to(3) }
+  end
 end
